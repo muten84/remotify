a37f52a1a20767d85b1f5c2a993dd8b577030436
Luigi Bifulco <bifulcoluigi@gmail.com>
2015-04-28 00:03:13 +0200

    fixed bug of IO.ImageRead to force correct image reading

--- xuggler-poc/src/main/java/it/luigibifulco/xuggler/ServerSocketImageReceiver.java
+++ xuggler-poc/src/main/java/it/luigibifulco/xuggler/ServerSocketImageReceiver.java
@@ -7,10 +7,13 @@ import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
+import java.math.BigInteger;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -33,12 +36,14 @@ public class ServerSocketImageReceiver {
 		viewer = new ImageListenerViewer();
 	}
 
-	public void startUI() throws InterruptedException, InvocationTargetException {
+	public void startUI() throws InterruptedException,
+			InvocationTargetException {
 		System.out.println("start UI");
 		viewer.start();
 	}
 
-	public void start(int port) throws IOException, InterruptedException, InvocationTargetException {
+	public void start(int port) throws IOException, InterruptedException,
+			InvocationTargetException {
 		System.out.println("starting server with port: " + port);
 		serverSocket = new ServerSocket(port);
 		System.out.println("accepting connection");
@@ -47,22 +52,43 @@ public class ServerSocketImageReceiver {
 		InputStream stream = socket.getInputStream();
 		ImageInputStream imageIs = null;
 		while (true) {
-			imageIs = new MemoryCacheImageInputStream(stream);
-//			if (!(stream.available() > 0)) {
-//				// System.out.println("Stream not available");
-//				Thread.sleep(50);
-//				continue;
-//			}
-
-			System.out.println("reading...");
-			BufferedImage image = ImageIO.read(imageIs);
-			System.out.println("image read... ");
-			if (image == null) {
-				// stream.reset();
+			char header1 = (char) stream.read();
+			char header2 = (char) stream.read();
+			int size = 0;
+			BigInteger bigI = null;
+			if (header1 == '#' && header2 == '#') {
+				size = (int) stream.read();
+				byte[] buffSize = new byte[size];
+				stream.read(buffSize);
+				try{
+				bigI = new BigInteger(buffSize);
+				}catch(Exception e){
+					
+				}
+			} else {
 				continue;
 			}
-			addFrame(image);
-			Thread.sleep(50);
+			char header3 = (char) stream.read();
+			if(header3!='#'){
+				continue;
+			}
+				
+			if (bigI != null) {			
+				imageIs = new MemoryCacheImageInputStream(stream);
+				BufferedImage image = null;
+				try {
+					image = ImageIO.read(imageIs);
+				} catch (Exception e) {
+					e.printStackTrace();
+					image = null;
+				}
+				System.out.println("image read... ");
+				if (image == null) {
+					System.out.println("image is null :(");
+					continue;
+				}
+				addFrame(image);
+			}
 		}
 	}
 
@@ -88,14 +114,26 @@ public class ServerSocketImageReceiver {
 
 		public void sendImage(BufferedImage image) throws IOException {
 			System.out.println("Sending image");
-			imageOs = new MemoryCacheImageOutputStream(os);
+			ByteArrayOutputStream byteOs = new ByteArrayOutputStream();
+			imageOs = new MemoryCacheImageOutputStream(byteOs);
 			boolean written = false;
 			try {
 				written = ImageIO.write(image, "jpg", imageOs);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			System.out.println("Image sent: " + written + " - " + imageOs.length());
+			byte[] imageBytes = byteOs.toByteArray();
+			System.out.println("Image sent: " + written + " - "
+					+ imageOs.length() + " - " + imageBytes.length);
+			BigInteger i = BigInteger.valueOf(imageOs.length());
+			byte[] length = i.toByteArray();
+			os.write((char) '#');
+			os.write((char) '#');
+			os.write(length.length);
+			os.write(length);
+			os.write((char) '#');
+			os.write(imageBytes);
+			os.flush();
 			// imageOs.flush();
 			// os.flush();
 
@@ -104,7 +142,9 @@ public class ServerSocketImageReceiver {
 
 	}
 
-	public static void main(String[] args) throws AWTException, UnknownHostException, IOException, InterruptedException, InvocationTargetException {
+	public static void main(String[] args) throws AWTException,
+			UnknownHostException, IOException, InterruptedException,
+			InvocationTargetException {
 		final ServerSocketImageReceiver server = new ServerSocketImageReceiver();
 		server.startUI();
 		new Thread(new Runnable() {
@@ -156,17 +196,20 @@ public class ServerSocketImageReceiver {
 				}
 				while (true) {
 					System.out.println(">>>>>>>>>>>>>>><Resending screenshot");
-					BufferedImage s = r.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+					BufferedImage s = r.createScreenCapture(new Rectangle(
+							Toolkit.getDefaultToolkit().getScreenSize()));
 					double scale = 0.75;
 					int w = (int) (s.getWidth() * scale);
 					int h = (int) (s.getHeight() * scale);
-					BufferedImage outImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+					BufferedImage outImage = new BufferedImage(w, h,
+							BufferedImage.TYPE_INT_RGB);
 					AffineTransform trans = new AffineTransform();
 					trans.scale(scale, scale);
 					Graphics2D g = outImage.createGraphics();
 					g.drawImage(s, trans, null);
 					g.dispose();
-					System.out.println(">>>>>>>>>>>>>>> invoking client for sending image");
+					System.out
+							.println(">>>>>>>>>>>>>>> invoking client for sending image");
 					try {
 						client.sendImage(outImage);
 					} catch (IOException e) {
