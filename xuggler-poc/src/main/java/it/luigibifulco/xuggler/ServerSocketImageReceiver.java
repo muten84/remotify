93418ab15880dd01543f319a0a4590ac413f3dd6
Luigi <bifulcoluigi@gmail.com>
2015-04-27 18:32:24 +0200

    test

--- xuggler-poc/src/main/java/it/luigibifulco/xuggler/ServerSocketImageReceiver.java
+++ xuggler-poc/src/main/java/it/luigibifulco/xuggler/ServerSocketImageReceiver.java
@@ -45,12 +45,25 @@ public class ServerSocketImageReceiver {
 		Socket socket = serverSocket.accept();
 		System.out.println("connection accepted");
 		InputStream stream = socket.getInputStream();
-		ImageInputStream imageIs = new MemoryCacheImageInputStream(stream);
-		System.out.println("reading...");
-		BufferedImage image = ImageIO.read(imageIs);
-		System.out.println("image read... ");
-		addFrame(image);
+		ImageInputStream imageIs = null;
+		while (true) {
+			imageIs = new MemoryCacheImageInputStream(stream);
+//			if (!(stream.available() > 0)) {
+//				// System.out.println("Stream not available");
+//				Thread.sleep(50);
+//				continue;
+//			}
 
+			System.out.println("reading...");
+			BufferedImage image = ImageIO.read(imageIs);
+			System.out.println("image read... ");
+			if (image == null) {
+				// stream.reset();
+				continue;
+			}
+			addFrame(image);
+			Thread.sleep(50);
+		}
 	}
 
 	public void addFrame(BufferedImage image) {
@@ -61,6 +74,7 @@ public class ServerSocketImageReceiver {
 	public static class Client {
 		private Socket socket = null;
 		private OutputStream os;
+		ImageOutputStream imageOs;
 
 		public Client() {
 
@@ -69,12 +83,23 @@ public class ServerSocketImageReceiver {
 		public void start(int port) throws UnknownHostException, IOException {
 			socket = new Socket("localhost", port);
 			os = socket.getOutputStream();
+			imageOs = new MemoryCacheImageOutputStream(os);
 		}
 
 		public void sendImage(BufferedImage image) throws IOException {
 			System.out.println("Sending image");
-			ImageOutputStream imageOs = new MemoryCacheImageOutputStream(os);
-			boolean written = ImageIO.write(image, "jps", imageOs);
+			imageOs = new MemoryCacheImageOutputStream(os);
+			boolean written = false;
+			try {
+				written = ImageIO.write(image, "jpg", imageOs);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			System.out.println("Image sent: " + written + " - " + imageOs.length());
+			// imageOs.flush();
+			// os.flush();
+
+			// imageOs.reset();
 		}
 
 	}
@@ -101,28 +126,62 @@ public class ServerSocketImageReceiver {
 			}
 		}).start();
 
-		Client client = new Client();
-		client.start(PORT);
+		final Client client = new Client();
+		new Thread(new Runnable() {
 
+			public void run() {
+				try {
+					client.start(PORT);
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		}).start();
+
+		new Thread(new Runnable() {
+
+			public void run() {
+				// TODO Auto-generated method stub
+				Robot r = null;
+				try {
+					r = new Robot();
+				} catch (AWTException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				while (true) {
+					System.out.println(">>>>>>>>>>>>>>><Resending screenshot");
+					BufferedImage s = r.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+					double scale = 0.75;
+					int w = (int) (s.getWidth() * scale);
+					int h = (int) (s.getHeight() * scale);
+					BufferedImage outImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+					AffineTransform trans = new AffineTransform();
+					trans.scale(scale, scale);
+					Graphics2D g = outImage.createGraphics();
+					g.drawImage(s, trans, null);
+					g.dispose();
+					System.out.println(">>>>>>>>>>>>>>> invoking client for sending image");
+					try {
+						client.sendImage(outImage);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					// System.out.println("Adding new frame...");
+					// viewer.addFrame(outImage);
+				}
+			}
+		}).start();
 		// ImageListenerViewer viewer = new ImageListenerViewer();
 		// viewer.start();
-		while (true) {
-			Robot r = new Robot();
-			BufferedImage s = r.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
-			double scale = 0.75;
-			int w = (int) (s.getWidth() * scale);
-			int h = (int) (s.getHeight() * scale);
-			BufferedImage outImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-			AffineTransform trans = new AffineTransform();
-			trans.scale(scale, scale);
-			Graphics2D g = outImage.createGraphics();
-			g.drawImage(s, trans, null);
-			g.dispose();
-			// Thread.sleep(1);
-			client.sendImage(outImage);
-			// System.out.println("Adding new frame...");
-			// viewer.addFrame(outImage);
-		}
+
 	}
 
 }
---
+++ xuggler-poc/src/main/java/it/luigibifulco/xuggler/UDPImageReceiver.java
@@ -0,0 +1,5 @@
+package it.luigibifulco.xuggler;
+
+public class UDPImageReceiver {
+
+}
